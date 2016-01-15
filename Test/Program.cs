c493bcaf2448d89db61e840ab83453032f76d7f9
zengen <lucky.zengen@gmail.com>
2016-01-16 00:04:23 +0800

    增加测试项目

---
+++ Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using z.Foundation.Data;
+using System.Configuration;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable dataTable = OracleHelper.ExecuteDataTable("PAPAGO", "SELECT * FROM T_IM_ITEM", System.Data.CommandType.Text, null);
+
+            Console.WriteLine(dataTable.Rows.Count);
+            Console.Read();
+        }
+    }
+}
--- Test/Properties/AssemblyInfo.cs
+++ Test/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("z.Foundation.Data")]
+[assembly: AssemblyTitle("Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("z.Foundation.Data")]
-[assembly: AssemblyCopyright("Copyright © zengen 2015")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("37e03fa3-0531-4a3a-9eaf-a0469b20f9f1")]
+[assembly: Guid("d6ff9297-d733-4e34-80d0-2af2204c8381")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.4")]
-[assembly: AssemblyFileVersion("1.0.0.4")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
