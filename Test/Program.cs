9e5eae6047747d3a1bb78cc911c061a061969edd
zengen <lucky.zengen@gmail.com>
2016-08-02 18:57:46 +0800

    TEST

--- Test/Program.cs
+++ Test/Program.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using z.Foundation.Data;
 using System.Configuration;
+using PanGu;
+using Lucene.Net.Analysis.PanGu;
 
 namespace Test
 {
@@ -13,10 +15,27 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            DataTable dataTable = OracleHelper.ExecuteDataTable("PAPAGO", "SELECT * FROM T_IM_ITEM", System.Data.CommandType.Text, null);
+            PanGu.Segment.Init(@"D:\project_about\file_server\pangu\PanGu.xml");
 
-            Console.WriteLine(dataTable.Rows.Count);
+            string strKeyword = GetKeyWordsSplitBySpace("荷兰牛栏奶粉1+800kg", new PanGuTokenizer());
+
+            
             Console.Read();
+        }
+
+        static string GetKeyWordsSplitBySpace(string keywords, PanGuTokenizer ktTokenizer)
+        {
+            StringBuilder result = new StringBuilder();
+            ICollection<WordInfo> words = ktTokenizer.SegmentToWordInfos(keywords);
+            foreach (WordInfo word in words)
+            {
+                if (word == null)
+                {
+                    continue;
+                }
+                result.AppendFormat("{0} ", word.Word);
+            }
+            return result.ToString().Trim();
         }
     }
 }
