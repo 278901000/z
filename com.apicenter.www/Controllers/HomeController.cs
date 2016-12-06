aba30fcd957da48c90742d740378ff544627de16
zhouy <278901000@qq.com>
2016-12-06 15:05:35 +0800

    页面调整

---
+++ com.apicenter.www/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace com.apicenter.www.Controllers
+{
+    public class HomeController : ApiBaseController
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
